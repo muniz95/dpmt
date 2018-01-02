134c226ea43ad7dad55fbd0da460e3f970b02a95
Rodrigo Muniz <muniz95@bsd.com.br>
2018-01-02 19:18:20 +0000

    Login da Intelly e Infojobs funcionando

--- src/lib/intelly.js
+++ src/lib/intelly.js
@@ -7,17 +7,6 @@ const URL = 'http://www.intelly.com.br'
 // const jar = new tough.CookieJar()
 
 const login = async (email, password) => {
-  const dataGet = {
-    'tipoFonte': 'arial',
-    'corFonte': '555d71',
-    'tamFonte': 11,
-    'tipoFonteTitulo': 'arial',
-    'corFonteTitulo': 'FF9966',
-    'tamFonteTitulo': 11,
-    'tipoFonteSubTitulo': 'arial',
-    'corFonteSubTitulo': 'FF9966',
-    'tamFonteSubTitulo': 11
-  }
   const form = {
     loginCad: true,
     tipoFonte: '',
@@ -33,54 +22,27 @@ const login = async (email, password) => {
     senha: password
   }
   
-  // const authPost = {
-  //   login: email,
-  //   senha: password
-  // }
-  
-  // const headers = {
-  //   'Content-Type': 'application/x-www-form-urlencoded'
-  // }
-  
-  var formData = querystring.stringify(form);
-  var contentLength = formData.length;
-  
-  const preOptions = {
-    uri: `${URL}/Extranet/loginCandidatoCad.do`
+  const headers = { 
+    'Content-Type' : 'application/x-www-form-urlencoded' 
   }
+  
   const options = {
     uri: `${URL}/Extranet/loginCandidato.do`,
     form,
     headers
   }
-  var headers = { 
-    'Content-Type' : 'application/x-www-form-urlencoded' 
-  };
+  
   const req = request.defaults({
     jar: true
   })
   
-  // await req.get(preOptions)
   
-  // await req.post(options)
-  req.post(options)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+  await req.post(options)
+  // req.post(options)
+  //   .then(res => console.log(res))
+  //   .catch(err => console.log(err))
 
   return req
-  
-  // instance.get(`/Extranet/loginCandidatoCad.do`, {params: dataGet})
-  //   .then(res => console.log(res.data))
-  //   .catch(err => console.error(err))
-  
-  // instance.post(`/Extranet/loginCandidato.do`, {
-  //   data: dataPost,
-  //   auth: authPost
-  // })
-  //   .then(res => console.log(res))
-  //   .catch(err => {
-  //     console.error(err.response.data)
-  //   })
 }
 
 // const get
